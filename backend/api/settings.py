4a5ad45e4df9d1296e44b82484ceb30db5df5775
shamsu <shamsubr@gmail.com>
2023-11-30 13:13:10 +0000

    First production faze

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -126,7 +126,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 # Static files (CSS, JavaScript, Images)
+
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
@@ -139,7 +142,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # cor allowed origin
 CORS_ALLOWED_ORIGINS = [
-    "http://sbinfohub.online",
+   
     "https://sbinfohub.online",
 ]
 
@@ -152,4 +155,4 @@ CKEDITOR_CONFIGS = {
 }
 
 CSRF_USE_SESSIONS = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
